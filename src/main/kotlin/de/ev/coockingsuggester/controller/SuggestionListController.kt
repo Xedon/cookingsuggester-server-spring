076ef7058ed63afb5606213cf4660e9eac6ff584
Emanuel Vollmer <xedon@xedon-coding.de>
2019-10-27 20:11:07 +0100

    Fix wrong Date format

--- src/main/kotlin/de/ev/coockingsuggester/controller/SuggestionListController.kt
+++ src/main/kotlin/de/ev/coockingsuggester/controller/SuggestionListController.kt
@@ -31,9 +31,9 @@ class SuggestionListController {
             produces = ["application/json"]
     )
     fun suggestions(
-            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
             @RequestParam("from") from: LocalDate,
-            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
             @RequestParam("to") to: LocalDate,
             pageable: Pageable
     ): PageImpl<CookingSuggestion> {
